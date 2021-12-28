58f5cb95bc200bc8e56e41ec5d557f4bc3e4d077
Wannes De Smet <wannes321@gmail.com>
2021-12-28 19:53:03 +0100

    Support any S3 compatible endpoint using minio mc

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-KEY=`jq -r .awskey /data/options.json`
-SECRET=`jq -r .awssecret /data/options.json`
-BUCKET=`jq -r .bucketname /data/options.json`
+KEY=$(jq -r .access_key /data/options.json)
+SECRET=$(jq -r .access_key /data/options.json)
+ENDPOINT=$(jq -r .endpoint /data/options.json)
+BUCKET=$(jq -r .bucket_name /data/options.json)
 
 now="$(date +'%d/%m/%Y - %H:%M:%S')"
 
-echo $now
+echo "$now"
 
-aws configure set aws_access_key_id $KEY
-aws configure set aws_secret_access_key $SECRET
+export MC_HOST_remote=https://$KEY:$SECRET@$ENDPOINT
 
-aws s3 sync /backup/ s3://$BUCKET/  --delete
+mc mirror /backup/ remote/"$BUCKET" --overwrite --remove
 
 echo "Done"
